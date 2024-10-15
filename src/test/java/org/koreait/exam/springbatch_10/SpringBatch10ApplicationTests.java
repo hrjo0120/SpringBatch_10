547cd4c82cd5722519a681658f21482903c462bd
hrjo0120 <hrjo0120@gmail.com>
2024-10-15 15:26:30 +0900

    프로젝트 세팅, 관련 스키마 생성

---
+++ src/test/java/org/koreait/exam/springbatch_10/SpringBatch10ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koreait.exam.springbatch_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
