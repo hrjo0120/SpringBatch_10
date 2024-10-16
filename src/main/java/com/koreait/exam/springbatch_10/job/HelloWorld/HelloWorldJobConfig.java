496127feef054484ac34da73362f24b4eb7164a1
hrjo0120 <hrjo0120@gmail.com>
2024-10-16 13:25:12 +0900

    Bean, Job, Step Scope

--- src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldJobConfig.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -17,7 +19,7 @@ public class HelloWorldJobConfig   {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    @Bean
+    @Bean // 프로그램 시작 시 태어남
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
                 .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행할 때 파라미터로 부여
@@ -25,11 +27,14 @@ public class HelloWorldJobConfig   {
     }
 
     @Bean
+    @JobScope // 생명주기 설정: 이 Step은 Job이 실행될 때 생성되고, Job이 끝나면 Step도 사라짐
     public Step helloWorldStep1() {
         return stepBuilderFactory.get("helloWorldStep1").tasklet(helloWorldTasklet()).build();
     }
 
     @Bean
+    @StepScope // 생명주기 설정: 이 Tasklet은 Step이 실행될 때 생성되고, Step이 종료되면 사라짐
+
     public Tasklet helloWorldTasklet() {
         return (stepContribution, chunkContext) -> {
             System.out.println("헬로월드!!!");
