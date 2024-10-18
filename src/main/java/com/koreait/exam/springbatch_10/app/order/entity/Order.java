a8fef30e81fd4a4181ec73acf1ca239718c0cfa4
hrjo0120 <hrjo0120@gmail.com>
2024-10-18 10:40:55 +0900

    도매가, 권장판매가, 소비자가, 노출용 사이즈, 색상

--- src/main/java/com/koreait/exam/springbatch_10/app/order/entity/Order.java
+++ src/main/java/com/koreait/exam/springbatch_10/app/order/entity/Order.java
@@ -33,3 +33,15 @@ public class Order extends BaseEntity {
         orderItems.add(orderItem);
     }
 }
+
+// 관계가 존재하면 필수
+// @ManyToOne : 다대일 관계
+// 다수의 엔티티가 하나의 엔티티에 연결됨
+// OrderItem(주문 항목) 은 Order(주문)dp thrgksek.
+//OrderItem이 -> Order를 참조해야해. 그래서 @ManyToOne 이다.
+
+// 필수는 아니지만, 관계를 맺고있는데 한쪽은 @ManyToOne 인데, @OneToMany가 안써있을 때, 파악하기 어렵다.
+// 관계를 명확히 할 때 써줌
+// @OneToMany : 일대다 관계
+// 하나의 엔티티가 여러 하위 엔티티를 가질 때 필수
+// 하나의 Order(주문)이 여러 OrderItem(주문 항목)을 가진다.
--- src/main/java/com/koreait/exam/springbatch_10/app/order/service/OrderService.java
+++ src/main/java/com/koreait/exam/springbatch_10/app/order/service/OrderService.java
@@ -23,40 +23,46 @@ public class OrderService {
     private final CartService cartService;
     private final OrderRepository orderRepository;
 
+    // 전달 받은 회원의 장바구니에 있는 아이템들을 전부 가져와서 주문으로 변환하는 과정
     public Order createFromCart(Member member) {
-        // 전달 받은 회원의 장바구니에 있는 아이템들을 전부 가져와
 
         // 만약에 장바구니의 특정 상품이 판매 불가 상태야 => 삭제
         // 만약에 장바구니의 특정 상품이 판매 가능 상태야 => 주문 품목으로 옮긴 후 삭제
-        List<CartItem> cartItems = cartService.getItemsByMember(member);
+        List<CartItem> cartItems = cartService.getItemsByMember(member);    // 회원이 장바구니에 담아 둔 모든 상품을 가져오는 것
 
         List<OrderItem> orderItems = new ArrayList<>();
 
-        for (CartItem cartItem : cartItems) {
-            ProductOption productOption = cartItem.getProductOption();
+        for (CartItem cartItem : cartItems) {   //cartItem 에 있는것을 가져와야함.
+            ProductOption productOption = cartItem.getProductOption();  //어떤 옵션인지 가져오고
 
-            if (productOption.isOrderable(cartItem.getQuantity())) {
-                orderItems.add(new OrderItem(productOption, cartItem.getQuantity()));
+            if (productOption.isOrderable(cartItem.getQuantity())) {    //주문한것인지 아닌지 가져와야함
+                orderItems.add(new OrderItem(productOption, cartItem.getQuantity()));   // 새로 올림
             }
 
-            cartService.deleteItem(cartItem);
+            cartService.deleteItem(cartItem);   // 장바구니에서 삭제되고
         }
 
-        return create(member, orderItems);
+        return create(member, orderItems);  // 주문은 따로 생성
     }
 
     @Transactional
     public Order create(Member member, List<OrderItem> orderItems) {
         Order order = Order.builder()
                 .member(member)
-                .build();
+                .build();       // 빌더패턴을 통한 객체 생성 = new와 같다, 데이터베이스에 저장하기위한 목적
 
+        // orderItem을 순회하면서 orderItems에 추가
         for (OrderItem orderItem : orderItems) {
             order.addOrderItem(orderItem);
         }
 
-        orderRepository.save(order);
+        orderRepository.save(order);    // 그리고 저장
 
         return order;
     }
 }
+
+// @Transactional(readOnly = true) : 스프링에서 트랜잭션 관리에 사용되는 어노테이션
+// 트랜잭션은 데이터베이스에서 여러 작업을 하나의 단위로 묶어 처리하는 기능을 의미하며, 이 과정에서 모든 작업이 성공적으로 완료되거나, 하나라도 실패하면 모든 작업이 취소(rollback)되어야 함
+// @Transactional
+// 데이터의 일관성을 유지하는 것 중 하나
